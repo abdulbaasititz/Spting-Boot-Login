fe1718084b09e573fae1dbae0d40f44130a7e652
abdulbaasititz <itzabdulbaasit@gmail.com>
2021-05-09 11:59:40 +0530

    login api

--- src/main/java/com/itz/login/LoginApplication.java
+++ src/main/java/com/itz/login/LoginApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +19,23 @@ public class LoginApplication {
 }
 @RestController
 @RequestMapping("/api")
-class pingController{
+class PingController{
 	@GetMapping("/ping")
 	public ResponseEntity<?> pingTest(){
 		Map<String,Object> statusOb = new HashMap<>();
 		statusOb.put("ping",1);
 		return new ResponseEntity<>(statusOb, HttpStatus.OK);
+	}
+}
+
+@RestController
+@RequestMapping("/api")
+class LoginCheck{
+	@RequestMapping(value="/login",method = RequestMethod.POST)
+	public ResponseEntity<?> loginCheckMethod(@RequestBody LoginDao loginDao){
+		if(loginDao.getUsername().equals("admin") && loginDao.getPassword().equals("admin")){
+			return new ResponseEntity<>("1",HttpStatus.OK);
+		}
+		return new ResponseEntity<>("-1", HttpStatus.BAD_REQUEST);
 	}
 }
---
+++ src/main/java/com/itz/login/LoginDao.java
@@ -0,0 +1,9 @@
+package com.itz.login;
+
+import lombok.Data;
+
+@Data
+public class LoginDao {
+    private String username;
+    private String password;
+}
